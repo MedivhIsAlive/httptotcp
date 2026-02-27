57b0260aa75dd5356509bcdd3916f1cf6b6fd345
Rinat Hashymov <mynameismedivh@gmail.com>
2026-02-27 12:47:19 +0100

    refactor a bit; added custom writer struct to give user flexibility

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"medivhtcp/internal/request"
 	"medivhtcp/internal/response"
@@ -13,24 +13,70 @@ import (
 
 const port = 42069
 
+func respond400() []byte {
+	return []byte(`
+			 <html>
+			  <head>
+				<title>400 Bad Request</title>
+			  </head>
+			  <body>
+				<h1>Bad Request</h1>
+				<p>Your request honestly kinda sucked.</p>
+			  </body>
+			</html>
+		`)
+}
+
+func respond200() []byte {
+	return []byte(`
+			 <html>
+			  <head>
+				<title>200 OK</title>
+			  </head>
+			  <body>
+				<h1>Success!</h1>
+				<p>Your request was an absolute banger.</p>
+			  </body>
+			</html>
+`)
+}
+
+func respond500() []byte {
+	return []byte(`
+		    <html>
+			  <head>
+				<title>500 Internal Server Error</title>
+			  </head>
+			  <body>
+				<h1>Internal Server Error</h1>
+				<p>Okay, you know what? This one is on me.</p>
+			  </body>
+			</html>
+`)
+}
+
 func main() {
-	s, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+	s, err := server.Serve(port, func(w *response.Writer, req *request.Request) *server.HandlerError {
+		h := response.GetDefaultHeaders(0)
+		body := respond200()
+		status := response.StatusOK
+
 		switch req.RequestLine.RequestTarget {
 		case "/yourproblem":
-			return &server.HandlerError{
-				StatusCode: response.StatusInternalServerError,
-				Message:    "Your problem is not my problem\n",
-			}
+			body = respond400()
+			status = response.StatusBadRequest
 		case "/myproblem":
-			return &server.HandlerError{
-				StatusCode: response.StatusInternalServerError,
-				Message:    "Woopsie, my bad\n",
-			}
-		default:
-			w.Write([]byte("All good, frfr\n"))
+			body = respond500()
+			status = response.StatusInternalServerError
 		}
+		h.Replace("content-length", fmt.Sprintf("%d", len(body)))
+		h.Replace("content-type", "text/html; charset=utf-8")
+		w.WriteStatusLine(status)
+		w.WriteHeaders(*h)
+		w.WriteBody(body)
 		return nil
 	})
+
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
--- internal/response/response.go
+++ internal/response/response.go
@@ -26,17 +26,15 @@ func GetDefaultHeaders(contentLen int) *headers.Headers {
 	return h
 }
 
-func WriteHeaders(w io.Writer, h *headers.Headers) error {
-	b := []byte{}
-	h.ForEach(func(n, v string) {
-		b = fmt.Appendf(b, "%s: %s\r\n", n, v)
-	})
-	b = fmt.Append(b, "\r\n")
-	_, err := w.Write(b)
-	return err
+type Writer struct {
+	writer io.Writer
 }
 
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+func NewWriter(writer io.Writer) *Writer {
+	return &Writer{writer: writer}
+}
+
+func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
 	statusLine := []byte{}
 	switch statusCode {
 	case StatusOK:
@@ -50,6 +48,22 @@ func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
 	}
 
 	statusLine = fmt.Appendf(statusLine, "\r\n")
-	_, err := w.Write(statusLine)
+	_, err := w.writer.Write(statusLine)
 	return err
+}
+
+func (w *Writer) WriteHeaders(headers headers.Headers) error {
+	b := []byte{}
+	headers.ForEach(func(n, v string) {
+		b = fmt.Appendf(b, "%s: %s\r\n", n, v)
+	})
+	b = fmt.Append(b, "\r\n")
+	_, err := w.writer.Write(b)
+	return err
+}
+
+func (w *Writer) WriteBody(p []byte) (int, error) {
+	n, err := w.writer.Write(p)
+
+	return n, err
 }
